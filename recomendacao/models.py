86101c4bc62c9d166f184b5af070f1476e801bee
Laercio Souza <laercioigorps@gmail.com>
2023-01-10 19:17:12 -0300

    adiciona fator de consumo relacionado ao peso

--- recomendacao/models.py
+++ recomendacao/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from perfil.models import Perfil
+from abc import ABC, abstractmethod
 
 # Create your models here.
+
+class IFatorDeConsumoDeAgua(ABC):
+
+    @abstractmethod
+    def calcular(perfil: Perfil) -> float:
+        pass
+
+
+class FatorDeConsumoPeso:
+    def __init__(self) -> None:
+        self.indice = 0.75
+
+    def calcular(self, perfil: Perfil) -> float:
+        return self.indice * perfil.peso
--- recomendacao/tests/test_models.py
+++ recomendacao/tests/test_models.py
@@ -1,1 +1,11 @@
 import pytest
+from perfil.models import Perfil
+from ..models import FatorDeConsumoPeso
+
+def test_fator_de_consumo_de_agua_peso():
+    perfil = Perfil(peso=75)
+    fator_de_consumo = FatorDeConsumoPeso()
+    assert hasattr(fator_de_consumo, "indice")
+    #este indice indica que o calculo de consumo de água recomendado é de 0.35ml/kg.
+    fator_de_consumo.indice = 0.35
+    assert fator_de_consumo.calcular(perfil) == 26.25
