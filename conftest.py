cc8413e28a2a75711a54e332ce7dabf482844623
Laercio Souza <laercioigorps@gmail.com>
2023-01-11 05:53:50 -0300

    atualiza usuarioComPerfil fixture para gerar recomendacão e meta de consumo de água

--- conftest.py
+++ conftest.py
@@ -3,6 +3,7 @@ import pytest
 from rest_framework.test import APIRequestFactory, APIClient
 from django.contrib.auth import get_user_model
 from perfil.models import Perfil, User
+from recomendacao.models import GuiaDeHidratacaoPessoal
 
 
 @pytest.fixture
@@ -17,5 +18,10 @@ def apiClient():
 @pytest.fixture
 def usuarioComPerfil(db):
     perfil = Perfil.objects.create(peso=70.20)
+    perfil.guia = GuiaDeHidratacaoPessoal.objects.create()
+    recomendacao = perfil.guia.calcular_recomendacao(perfil)
+    perfil.guia.recomendacao = recomendacao
+    perfil.guia.meta = recomendacao
+    perfil.save()
     user = get_user_model().objects.create(username="random", perfil=perfil)
     return user
